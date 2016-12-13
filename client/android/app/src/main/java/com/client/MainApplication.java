8f3340d44f38d592a3a795984cff5b02fabbb032
rogueSnake <jculbertsonfaegre@gmail.com>
2016-12-12 20:21:17 -0600

    Building react-native for Android successfully.

--- client/android/app/src/main/java/com/client/MainApplication.java
+++ client/android/app/src/main/java/com/client/MainApplication.java
@@ -1,4 +1,4 @@
-package com.dtom;
+package com.client;
 
 import android.app.Application;
 import android.util.Log;
--- client/ios/clientTests/clientTests.m
+++ client/ios/clientTests/clientTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface dtomTests : XCTestCase
+@interface clientTests : XCTestCase
 
 @end
 
-@implementation dtomTests
+@implementation clientTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
